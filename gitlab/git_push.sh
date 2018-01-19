73d3acc9aed48c6bdfb787fa6e3575f022547470
AbreezaSaleem <abreeza.saleem@hotmail.com>
2018-01-19 21:39:03 +0500

    git_push

--- gitlab/git_push.sh
+++ gitlab/git_push.sh
@@ -4,4 +4,4 @@ workDir=$2
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $scriptDir/$workDir
-ssh-agent bash -c "ssh-add $scriptDir/keys/$userName.rsa; git add .; git commit -m 'adding new'; git push origin master"
+ssh-agent bash -c "ssh-add $scriptDir/keys/$userName.rsa; git add .; git commit -m 'git_push'; git push origin master"
